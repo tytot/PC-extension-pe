eba82cf25e671c9dd27e60d7eba3ae8c66dac4e2
Tyler <tylerpotato@outlook.com>
2016-08-04 12:48:06 -0400

    Revisions

--- src/Tytot/AutoRestart/Main.php
+++ src/Tytot/AutoRestart/Main.php
@@ -5,6 +5,7 @@ namespace Tytot\AutoRestart;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+  public $min = 10;
   public function onEnable(){
     $this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this), 20 * 60); //every minute
   }

ccb65e35a6d480067e04fa13a2e27e0305d06da1
Tyler <tylerpotato@outlook.com>
2016-08-04 13:08:28 -0400

    Changed to seconds
    For compatibility with the timer

--- src/Tytot/AutoRestart/Main.php
+++ src/Tytot/AutoRestart/Main.php
@@ -5,8 +5,8 @@ namespace Tytot\AutoRestart;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
-  public $min = 10;
-  public function onEnable(){
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this), 20 * 60); //every minute
-  }
+	public $seconds = 10;
+	public function onEnable(){
+    		$this->getServer()->getScheduler()->scheduleRepeatingTask(new Timer($this), 20); //every second
+  	}
 }
